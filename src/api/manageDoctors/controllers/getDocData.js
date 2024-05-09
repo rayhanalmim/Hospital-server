ebb67b18d8da93b450b3e227be8a04f9ebfc0c7f
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-09 14:46:32 +0600

    meke a search functionality for getting doctors data

--- src/api/manageDoctors/controllers/getDocData.js
+++ src/api/manageDoctors/controllers/getDocData.js
@@ -1,7 +1,26 @@
 const Doctor = require("../../../models/doctorsSchema");
 
 const getDoc = async (req, res) => {
-  const data = await Doctor.find();
-  res.send(data);
+          let query = {};
+      
+          if (req.query.hospital_area) {
+            query.hospital_area = { $regex: new RegExp(req.query.hospital_area, "i") };
+          }
+      
+          if (req.query.designation) {
+            query.designation = { $regex: new RegExp(req.query.designation, "i") };
+          }
+      
+          if (req.query.hospital_name) {
+            query.hospital_name = { $regex: new RegExp(req.query.hospital_name, "i") };
+          }
+      
+          if(!req.query.hospital_area && !req.query.designation && !req.query.hospital_name){
+            const doctors = await Doctor.find();
+           return res.send(doctors);
+          }
+          const doctors = await Doctor.find(query).sort({ name: 1 });
+      
+          res.json(doctors);
 };
 module.exports = getDoc;
