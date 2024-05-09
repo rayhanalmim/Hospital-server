9b8277818a697666e9ba425b59641dee8266f4ad
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-09 13:49:52 +0600

    created server using mongoose and connect the server with mongodb

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const connectDB = require("./db/connectDB");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+    res.send("Expo Elite Server is Running");
+  });
+  
+  app.all("*", (req, res, next) => {
+    const error = new Error(`The Requested URL is Invalid: [${req.url}]`);
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+    });
+  });
+  
+  const main = async () => {
+    await connectDB();
+    app.listen(port, (req, res) => {
+      console.log(`Expo Elite Server running On Port: ${port}`);
+    });
+  
+  
+  };
+  
+  main();
