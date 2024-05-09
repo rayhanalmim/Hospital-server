42ea5701f1dc966a326c7c34d27d5e83e0585f82
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-09 14:21:31 +0600

    require and use cors , and connect server with front end

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 
+app.use(cors());
+app.use(express.json({ limit: '10mb' })); 
+
 const doctor = require("./routes/doctor/index");
 app.use(doctor);
 
